a9caa30913e367b8216b9adfcee87b45699ca625
Andrea Bonomi <andrea.bonomi@gmail.com>
2025-03-21 23:31:18 +0100

    Parse query parameters

--- meca-web/js/App.vue
+++ meca-web/js/App.vue
@@ -93,7 +93,60 @@ export default defineComponent({
         let data = get_initial_state();
         data.seed = new Date().getTime();
         data.error = null;
+        // Parse query parameters
+        const params = new URLSearchParams(window.location.search);
+        if (params.has('r')) {
+            data.rule = parseInt(params.get('r'));
+        }
+        if (params.has('f')) {
+            data.rule_type = params.get('f');
+        }
+        if (params.has('s')) {
+            data.steps = parseInt(params.get('s'));
+        }
+        if (params.has('w')) {
+            data.size = parseInt(params.get('w'));
+        }
+        if (params.has('b')) {
+            data.boundaries = params.get('b');
+        }
+        if (params.has('u')) {
+            data.update_pattern = params.get('u');
+        }
+        if (params.has('i')) {
+            data.initial_state = params.get('i');
+        }
+        if (params.has('ii')) {
+            data.custom_initial_state = params.get('ii');
+        }
+        if (params.has('a')) {
+            data.alpha = parseFloat(params.get('a'));
+        }
+        if (params.has('n')) {
+            data.set_random_seed = true;
+            data.seed = parseInt(params.get('n'));
+            if (isNaN(data.seed)) {
+                data.seed = 0;
+            }
+            data.seed_hex_str = data.seed.toString(16);
+        }
+        if (params.has('c')) {
+            data.color_scheme = params.get('c');
+        }
+        if (params.has('cs')) {
+            data.cell_size = parseInt(params.get('cs'));
+        }
         return data;
+    },
+    created() {
+        window.addEventListener('keydown', (e) => {
+            if (e.key == 'Escape') {
+                this.toggle();
+            } else if (e.key == ' ') {
+                this.run();
+            }
+
+        });
     },
     computed: {
         useRandom() {
@@ -108,19 +161,47 @@ export default defineComponent({
                 if (!this.set_random_seed) {
                     this.seed_hex_str = this.seed.toString(16);
                 }
+                this.updateQueryParameters();
                 this.error = null;
-                const stats = draw(canvas, this, 800, 600);
+                const stats = draw(canvas, this);
                 console.log(stats);
             } catch (ex) {
                 this.error = ex;
                 console.log(ex);
             }
         },
+        updateQueryParameters() {
+            // Update query parameters
+            const params = new URLSearchParams();
+            params.set('r', this.rule);
+            params.set('f', this.rule_type);
+            params.set('s', this.steps);
+            params.set('w', this.size);
+            params.set('b', this.boundaries);
+            if (this.update_pattern.endsWith('<n>')) {
+                params.set('u', this.update_pattern.slice(0, -3) + this.update_pattern_number);
+            } else {
+                params.set('u', this.update_pattern);
+            }
+            params.set('i', this.initial_state);
+            params.set('ii', this.custom_initial_state);
+            params.set('a', this.alpha);
+            if (this.set_random_seed) {
+                params.set('n', parseInt(this.seed_hex_str, 16));
+            } else {
+                params.set('n', this.seed);
+            }
+            params.set('c', this.color_scheme);
+            params.set('cs', this.cell_size);
+            window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
+        },
         toggle() {
+            // Show/hide window content
             const content = this.$refs.content;
             content.style.display = content.style.display == 'none' ? 'block' : 'none';
         },
         dragStart(e) {
+            // Start dragging the window
             e = e || window.event;
             e.preventDefault();
             this.x = e.clientX;
@@ -129,6 +210,7 @@ export default defineComponent({
             document.onmousemove = this.dragElement;
         },
         dragElement(e) {
+            // Drag the window
             e = e || window.event;
             e.preventDefault();
             const dx = this.x - e.clientX;
@@ -140,6 +222,7 @@ export default defineComponent({
             this.y = e.clientY;
         },
         dragStop() {
+            // Stop dragging the window
             document.onmouseup = null;
             document.onmousemove = null;
         }
